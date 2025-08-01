5da28e73170ac7b8b894c5880d562faf8968d11b
manthan <m@gmail.com>
2025-08-01 18:36:34 +0530

    photo change

--- src/components/CampaignForm.jsx
+++ src/components/CampaignForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 import './CampaignForm.css';
-import icon from '../assets/icon.jpeg';
+import icon from '../assets/icon.png';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
